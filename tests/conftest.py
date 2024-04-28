3c78dede63e31178dd8ed67ec51f607df27a75fb
dmitryhusev <dimtri.gusev@gmail.com>
2024-04-29 01:36:42 +0200

    trace + screen

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,24 @@
 import pytest
-from playwright.sync_api import Page
+import os
+
 
 @pytest.fixture
-def browser(page: Page, request):
+def browser(context, request):
+    page = context.new_page()
+    if request.node.execution_count == 2:
+        context.tracing.start(screenshots=True, snapshots=True)
+
     yield page
-    if request.node.session.testsfailed > 0:
-        page.screenshot(path=f"{request.node.name}.png")
+    if request.node.execution_count == 2 and request.node.rep_call.passed == False:
+        path_to_res = f"./output/{request.node.name}"
+        try:
+            os.makedirs(path_to_res)
+        except FileExistsError:
+            pass
+        page.screenshot(path=f"{path_to_res}/screenshot.png")
+        context.tracing.stop(path = f"{path_to_res}/trace.zip")
+    page.close()
+    context.close()
+
 
 
